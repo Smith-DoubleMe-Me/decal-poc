d763794403a6e499dba311f5a6710039a1df4fc2
smith-doubleme-me <smith.jang@doubleme.me>
2024-02-23 11:40:40 +0900

    sharp engine 관련 문제 해결

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config, { isServer }) {
+    config.externals.push('sharp');
+
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif|glb|gltf|usdz)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+          name: '[name].[ext]',
+        },
+      },
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import ModelViewer from './components/3dviewer/ModelViewer';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ModelViewer />
+      <div className="relative flex h-[438px] w-[588px] items-center justify-center overflow-hidden rounded-3xl border">
+        <ModelViewer />
+      </div>
     </main>
   );
 }
