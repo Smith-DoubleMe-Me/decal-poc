07439c06446e324929c274143d3dc987e1a40d5d
smith-doubleme-me <smith.jang@doubleme.me>
2024-02-23 16:31:50 +0900

    test

--- src/app/model/[id]/page.tsx
+++ src/app/model/[id]/page.tsx
@@ -9,25 +9,25 @@ type Props = {
 
 type IdProps = 'deer' | 'export' | 'model2' | 'model4';
 
-export async function generateMetadata(
-  { params, searchParams }: Props,
-  parent: ResolvingMetadata,
-): Promise<Metadata> {
-  const id = params.id;
+// export async function generateMetadata(
+//   { params, searchParams }: Props,
+//   parent: ResolvingMetadata,
+// ): Promise<Metadata> {
+//   const id = params.id;
 
-  const { url } = searchParams;
+//   const { url } = searchParams;
 
-  const previousImages = (await parent).openGraph?.images || [];
+//   const previousImages = (await parent).openGraph?.images || [];
 
-  return {
-    metadataBase: new URL('https://decal-poc.vercel.app/'),
-    title: id,
-    // openGraph: {
-    //   images: [url, ...previousImages],
-    // },
-    description: 'DECAL 3D Model rendering',
-  };
-}
+//   return {
+//     metadataBase: new URL('https://decal-poc.vercel.app/'),
+//     title: id,
+//     // openGraph: {
+//     //   images: [url, ...previousImages],
+//     // },
+//     description: 'DECAL 3D Model rendering',
+//   };
+// }
 
 export default function Home({ params, searchParams }: Props) {
   const query = params.id as IdProps;
