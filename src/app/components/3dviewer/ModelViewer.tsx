1b24618718ce9400f8db4d64c48a1fbc6815414f
smith-doubleme-me <smith.jang@doubleme.me>
2024-02-28 11:12:21 +0900

    style: loading component 추가

--- src/app/components/3dviewer/ModelViewer.tsx
+++ src/app/components/3dviewer/ModelViewer.tsx
@@ -1,14 +1,27 @@
 'use client';
 
-import { OrbitControls, Stage, Bounds } from '@react-three/drei';
+import { OrbitControls, Stage, Bounds, Html } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { uuid } from '@gltf-transform/core';
+import { Suspense } from 'react';
 
 import Model from './Model';
 
 type Props = {
   id: 'deer' | 'export' | 'model2' | 'model4';
 };
+
+const ModelLoader = () => (
+  <Html center>
+    <div
+      className="text-landing inline-block h-60 w-60 animate-spin rounded-full border-[3px] border-current border-t-transparent"
+      role="status"
+      aria-label="loading"
+    >
+      <span className="sr-only">Loading...</span>
+    </div>
+  </Html>
+);
 
 export default function ModelViewer({ id }: Props) {
   return (
@@ -20,11 +33,13 @@ export default function ModelViewer({ id }: Props) {
     >
       <color attach="background" args={[196, 196, 196]} />
       <ambientLight />
-      <Stage adjustCamera environment={'sunset'}>
-        <Bounds fit clip observe margin={1}>
-          <Model url={`../${id}.glb`} />
-        </Bounds>
-      </Stage>
+      <Suspense fallback={<ModelLoader key={id} />}>
+        <Stage adjustCamera environment={'sunset'}>
+          <Bounds fit clip observe margin={1}>
+            <Model url={`../${id}.glb`} />
+          </Bounds>
+        </Stage>
+      </Suspense>
       <OrbitControls makeDefault />
     </Canvas>
   );
