3e859f2e6c1b0e2ff93bceae2c7c19aaa99c66fa
smith-doubleme-me <smith.jang@doubleme.me>
2024-02-28 10:59:53 +0900

    feat: model list routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,48 @@
-import ModelViewer from './components/3dviewer/ModelViewer';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const IMAGE_LIST = [
+  {
+    id: 'deer',
+    src: '/deer.png',
+  },
+  {
+    id: 'export',
+    src: '/export.png',
+  },
+  {
+    id: 'model2',
+    src: '/model2.png',
+  },
+  {
+    id: 'model4',
+    src: '/model4.png',
+  },
+];
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative flex h-[550px] w-[620px] items-center justify-center overflow-hidden rounded-3xl border">
-        {/* <ModelViewer /> */}
+      <div className="h-fit w-[820px] overflow-hidden">
+        <ul className="flex flex-wrap items-center justify-center gap-x-4 gap-y-10">
+          {IMAGE_LIST.map((image) => {
+            return (
+              <li key={image.id} className="flex flex-col items-center">
+                <Image
+                  src={image.src}
+                  alt={image.id}
+                  width={400}
+                  height={400}
+                />
+                <div>
+                  <Link href={`/model/${image.id}`} className="hover:font-bold">
+                    View the model
+                  </Link>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </main>
   );
